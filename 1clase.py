bf7393be5ad6b0f5e309fa648dc22201823214a0
JuanMarin <juanesteban24082005+3@gmail.com>
2023-11-14 15:32:02 -0500

    subida de los primeros 2 archivos

---
+++ 1clase.py
@@ -0,0 +1,23 @@
+print("Hello world")
+print("Hola soy juan, y tengo 18 años")
+
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(5%5)
+
+#comentario en una linea
+
+"""
+comentario
+en
+muchas
+lineas
+"""
+'''
+otro
+comentarios
+en varias
+lineas
+'''
---
+++ variables.py
@@ -0,0 +1,17 @@
+#crear e imprimir una variable
+name = "juan"
+print(name)
+age = 18
+print(age)
+
+#concatener la variable
+print("su nombre es",name,"su edad es",age)
+
+#cambiar valor de una variable
+name = "santiago"
+age = 22
+print("nombre",name,"edad",age)
+
+#ingresar datos con input
+name = input("Ingrese el nombre ")
+print("nombre usando el input",name)
